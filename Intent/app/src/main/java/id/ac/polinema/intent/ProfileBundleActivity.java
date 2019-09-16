a80ce77579af4321bd6672544174778b2be53219
aflahbrilli <aflahbrilli891@gmail.com>
2019-09-16 17:26:09 +0700

    tugas intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,8 +3,13 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +18,22 @@ public class ProfileBundleActivity extends AppCompatActivity {
 
         // TODO: bind here
 
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
+
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             // TODO: display value here
+
+            String username = extras.getString( "username");
+            String name = extras.getString( "name");
+            String age = String.valueOf(extras.getInt("age"));
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(age);
+
         }
     }
 }
